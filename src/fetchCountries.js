a89076e76db978f0b27981b73a0697207d243984
Viktor H <66110545+vsHonta@users.noreply.github.com>
2023-01-08 17:44:29 -0600

    debounce added

---
+++ src/fetchCountries.js
@@ -0,0 +1,4 @@
+export const fetchCountries = (name) => {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries'
 
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+
+
+const refs = {
+    input: document.querySelector("#search-box"),
+}
+const { input } = refs
+
+input.addEventListener('input', debounce(onInput, 300))
+    // debounce(() =>
+    //     fetchCountries(input.value)
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
+    //         .catch(err => console.log(err)), 1000))
+
+
+
+function onInput() {
+    if (input.value === "") {
+        return
+    }
+    fetchCountries(input.value)
+    .then(res => res.json())
+    .then(data => console.log(data))
+        .catch(err => console.log(err))
+}
